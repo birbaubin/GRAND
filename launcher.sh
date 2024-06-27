748eeb643426da96cc79c6af82525284552ff2bc
birbaubin <aubin.birba@gmail.com>
2024-06-27 10:31:43 -0400

    WIP Erdos + deterministic

--- launcher.sh
+++ launcher.sh
@@ -1,3 +1,3 @@
 python=python3.9
 
-$python main.py --dataset polblogs --heuristic preferential_attachment --number_of_experiments 5 --addition_proportion 1e-3 --graph1_props 0 --prediction_type A
+$python deterministic_erdos.py --dataset polblogs --heuristic preferential_attachment --number_of_experiments 5 --addition_proportion 1e-3 --graph1_props 0.1 --prediction_type A
