68f6187b0db0e33c36e2c4fc92c9d58b9ba68b81
birbaubin <aubin.birba@gmail.com>
2024-10-12 10:43:34 -0400

    gamma experiments

--- launcher.sh
+++ launcher.sh
@@ -4,23 +4,20 @@ python_exe=python3.9
 $python_exe benchmark.py --dataset bio-diseasome --type D  --n_experiments 1
 $python_exe benchmark.py --dataset bio-diseasome --type P --n_experiments 1
 
-$python_exe benchmark.py --dataset bio-diseasome --type DP --proba_params  0 1 0 --n_experiments 1
-$python_exe benchmark.py --dataset bio-diseasome --type DP --proba_params  0.1 0.9 0 --n_experiments 1
-$python_exe benchmark.py --dataset bio-diseasome --type DP --proba_params  0.25 0.75 0 --n_experiments 1
-$python_exe benchmark.py --dataset bio-diseasome --type DP --proba_params  0.5 0.5 0 --n_experiments 1
+$python_exe benchmark.py --dataset bio-diseasome --type DP --proba_params  0 0 1 --n_experiments 1
+$python_exe benchmark.py --dataset bio-diseasome --type DP --proba_params  0.05 0.05 0.9 --n_experiments 1
+$python_exe benchmark.py --dataset bio-diseasome --type DP --proba_params  0.125 0.125 0.75 --n_experiments 1
+$python_exe benchmark.py --dataset bio-diseasome --type DP --proba_params  0.25 0.25 0.5 --n_experiments 1
+$python_exe benchmark.py --dataset bio-diseasome --type DP --proba_params  0.75 0.125 0.125 --n_experiments 1
 $python_exe benchmark.py --dataset bio-diseasome --type DP --proba_params  0.75 0.25 0 --n_experiments 1
-$python_exe benchmark.py --dataset bio-diseasome --type DP --proba_params  0.9 0.1 0 --n_experiments 1
-$python_exe benchmark.py --dataset bio-diseasome --type DP --proba_params  1 0 0 --n_experiments 1
+# $python_exe benchmark.py --dataset bio-diseasome --type DP --proba_params  1 0 0 --n_experiments 1
 
-
-
-$python_exe benchmark.py --dataset bio-diseasome --type DPD --proba_params  0 1 0 --n_experiments 1
-$python_exe benchmark.py --dataset bio-diseasome --type DPD --proba_params  0.1 0.9 0 --n_experiments 1
-$python_exe benchmark.py --dataset bio-diseasome --type DPD --proba_params  0.25 0.75 0 --n_experiments 1
-$python_exe benchmark.py --dataset bio-diseasome --type DPD --proba_params  0.5 0.5 0 --n_experiments 1
+$python_exe benchmark.py --dataset bio-diseasome --type DPD --proba_params  0 0 1 --n_experiments 1
+$python_exe benchmark.py --dataset bio-diseasome --type DPD --proba_params  0.05 0.05 0.9 --n_experiments 1
+$python_exe benchmark.py --dataset bio-diseasome --type DPD --proba_params  0.125 0.125 0.75 --n_experiments 1
+$python_exe benchmark.py --dataset bio-diseasome --type DPD --proba_params  0.25 0.25 0.5 --n_experiments 1
+$python_exe benchmark.py --dataset bio-diseasome --type DPD --proba_params  0.75 0.125 0.125 --n_experiments 1
 $python_exe benchmark.py --dataset bio-diseasome --type DPD --proba_params  0.75 0.25 0 --n_experiments 1
-$python_exe benchmark.py --dataset bio-diseasome --type DPD --proba_params  0.9 0.1 0 --n_experiments 1
-$python_exe benchmark.py --dataset bio-diseasome --type DPD --proba_params  1 0 0 --n_experiments 1
 
 
 # $python_exe benchmark.py --dataset bio-diseasome --type DPD --proba_params 0.3 0.3 0.3 --n_experiments 1
