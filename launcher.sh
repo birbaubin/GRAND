12b598877a18135da79fe4db3a6eca44376d42f4
birbaubin <aubin.birba@gmail.com>
2024-11-23 00:33:59 -0500

    launcher added

--- launcher.sh
+++ launcher.sh
@@ -1,17 +1,19 @@
-python_exe=python
+python_exe=python3.9
 
 
-$python_exe benchmark.py --dataset random_100_0.05 --types H DDH_0.0_0.0_0_0 DDH_0.0_0.0_0_1 --graph1_props 0.0 1.0 0.05 --n_experiments 50
-$python_exe benchmark.py --dataset random_100_0.10 --types H DDH_0.0_0.0_0_0 DDH_0.0_0.0_0_1 --graph1_props 0.0 1.0 0.05 --n_experiments 20
-$python_exe benchmark.py --dataset random_100_0.25 --types H DDH_0.0_0.0_0_0 DDH_0.0_0.0_0_1 --graph1_props 0.0 1.0 0.05 --n_experiments 20
-$python_exe benchmark.py --dataset random_100_0.5 --types H DDH_0.0_0.0_0.0_0 DDH_0.0_0.0_0.0_1 --graph1_props 0.0 1.0 0.05 --n_experiments 20
+$python_exe benchmark.py --dataset netscience --types H DDH_0.0_0.0_0_0 DDH_0.0_0.0_0_1 --graph1_props 0.0 1.0 1 --n_experiments 1 --log_deterministic
+$python_exe benchmark.py --dataset bio-diseasome --types H DDH_0.0_0.0_0_0 DDH_0.0_0.0_0_1 --graph1_props 0.0 1.0 1 --n_experiments 1 --log_deterministic
+$python_exe benchmark.py --dataset polblogs --types H DDH_0.0_0.0_0_0 --graph1_props 0.0 1.0 1 --n_experiments 1 --log_deterministic
+$python_exe benchmark.py --dataset cora --types H DDH_0.0_0.0_0_0 --graph1_props 0.0 1.0 1 --n_experiments 1 --log_deterministic
 
 
-$python_exe benchmark.py --dataset barabasi_100_1 --types H DDH_0.0_0.0_0_0 DDH_0.0_0.0_0_1 --graph1_props 0.0 1.0 0.05 --n_experiments 20
-$python_exe benchmark.py --dataset barabasi_100_2 --types H DDH_0.0_0.0_0_0 DDH_0.0_0.0_0_1 --graph1_props 0.0 1.0 0.05 --n_experiments 20
-$python_exe benchmark.py --dataset barabasi_100_3 --types H DDH_0.0_0.0_0_0 DDH_0.0_0.0_0_1 --graph1_props 0.0 1.0 0.05 --n_experiments 20
-$python_exe benchmark.py --dataset barabasi_100_4 --types H DDH_0.0_0.0_0_0 DDH_0.0_0.0_0_1 --graph1_props 0.0 1.0 0.05 --n_experiments 20
 
+# $python_exe benchmark.py --dataset --types H DDH_0.0_0.0_0_0 DDH_0.0_0.0_0_1 --graph1_props 0.0 1.0 0.05 --n_experiments 20
+# $python_exe benchmark.py --dataset --types H DDH_0.0_0.0_0_0 DDH_0.0_0.0_0_1 --graph1_props 0.0 1.0 0.05 --n_experiments 20
+# $python_exe benchmark.py --dataset --types H DDH_0.0_0.0_0_0 DDH_0.0_0.0_0_1 --graph1_props 0.0 1.0 0.05 --n_experiments 20
+# $python_exe benchmark.py --dataset --types H DDH_0.0_0.0_0_0 DDH_0.0_0.0_0_1 --graph1_props 0.0 1.0 0.05 --n_experiments 20
+
+# $python_exe benchmark.py --dataset cora --types H DDH_0.0_0.0_0_0 --graph1_props 0.0 1.0 1.0 --n_experiments 1
 
 
 # $python_exe benchmark.py --dataset random_500_0.05 --types H DDH_0.0_0.0_0_0 DDH_0.0_0.0_0_1 --graph1_props 0.0 1.0 0.05 --n_experiments 50
